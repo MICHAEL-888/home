8d25742f7ac9a6cd3a9bb48f3210b8c342e9c2ff
MICHAEL-888 <3097847012@qq.com>
2024-01-26 20:40:16 +0800

    更新 GithubProject.vue

--- src/components/GithubProject.vue
+++ src/components/GithubProject.vue
@@ -1,1 +1,8 @@
-// ####
+<template>
+</template>
+
+<script setup>
+import { GithubOne, Bookmark } from "@icon-park/vue-next";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Pagination, Mousewheel } from "swiper";
+</script>
